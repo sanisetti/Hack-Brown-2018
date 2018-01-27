164f64bc6c2035913f36b1e0d4e1594f159a928d
seanrayment <rayment.code@gmail.com>
2018-01-27 15:55:00 -0500

    finds img tags

--- content.js
+++ content.js
@@ -1,1 +1,12 @@
-alert("Hello, world")
+$(document).ready(function () {
+	
+	console.log("Loaded Page")
+
+	$('img').each(function () {
+		console.log("found an image")
+		if (!$(this).attr('alt')) {
+			console.log("no alt!")
+		}
+	})
+
+})
