41c7bb00d2c07d845d074d2a162b83c0bf82a851
Sathya <sathya_anisetti@brown.edu>
2018-01-28 02:52:50 -0500

    Made the logs more informative, and also did some error handling

--- content.js
+++ content.js
@@ -17,28 +17,21 @@ function updateImages() {
 
 	$('*').each(function () {
 		if ($(this).is('img')) {
-			console.log(numRequests);
 			if(numRequests >= 5){
 				return;
 			}
 			var image = this;
-			console.log($(this).attr('src'))
+			console.log("Checking " + $(image).attr('src'))
 			if (!$(this).attr('alt')) {
-				console.log("no alt!")
+				console.log($(image).attr('src') + " has no alt!")
 				numRequests = numRequests + 1;
 				sourceImageUrl = $(this).attr('src');
-				console.log(sourceImageUrl);
-				 // Replace the subscriptionKey string value with your valid subscription key.
+				if (sourceImageUrl.indexOf("jpeg") < 0 && sourceImageUrl.indexOf("jpg") < 0 && sourceImageUrl.indexOf("png") < 0) {
+					console.log("image-type not currently supported")
+					return;
+				}
 				 var subscriptionKey = "6d4d15f30dcc42ba81e90db3b638b2ae";
 
-				 // Replace or verify the region.
-				 //
-				 // You must use the same region in your REST API call as you used to obtain your subscription keys.
-				 // For example, if you obtained your subscription keys from the westus region, replace
-				 // "westcentralus" in the URI below with "westus".
-				 //
-				 // NOTE: Free trial subscription keys are generated in the westcentralus region, so if you are using
-				 // a free trial subscription key, you should not need to change this region.
 				 var uriBase = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/analyze";
 
 				 // Request parameters.
@@ -47,10 +40,6 @@ function updateImages() {
 						 "details": "",
 						 "language": "en",
 				 };
-
-				 // Display the image.
-				 //var sourceImageUrl = document.getElementById("inputImage").value;
-				 //document.querySelector("#sourceImage").src = sourceImageUrl;
 
 				 // Perform the REST API call.
 				 $.ajax({
@@ -72,22 +61,26 @@ function updateImages() {
 						 // Show formatted JSON on webpage.
 						 //$("#responseTextArea").val(JSON.stringify(data, null, 2));
 						 var obj = JSON.parse(JSON.stringify(data, null, 2));
-						 console.log(obj.description.captions[0].text);
 						 $(image).attr('alt', obj.description.captions[0].text);
+						 console.log("Injected: " + obj.description.captions[0].text + " into " + $(image).attr('src'));
 				 })
 
 				 .fail(function(jqXHR, textStatus, errorThrown) {
 						 // Display error message.
 						 var errorString = (errorThrown === "") ? "Error. " : errorThrown + " (" + jqXHR.status + "): ";
 						 errorString += (jqXHR.responseText === "") ? "" : jQuery.parseJSON(jqXHR.responseText).message;
-						 console.log(errorString);
+						 //console.log(errorString);
 				 });
 			}
 			else {
 				if($(this).attr('alt').split(" ").length <= 2){
 					numRequests = numRequests + 1;
 					sourceImageUrl = $(this).attr('src');
-					console.log(sourceImageUrl);
+					console.log($(image).attr('src') + " has useless description")
+					if (sourceImageUrl.indexOf("jpeg") < 0 && sourceImageUrl.indexOf("jpg") < 0 && sourceImageUrl.indexOf("png") < 0) {
+						console.log("image-type not currently supported")
+						return;
+					}
 					 // Replace the subscriptionKey string value with your valid subscription key.
 					 var subscriptionKey = "6d4d15f30dcc42ba81e90db3b638b2ae";
 
@@ -132,15 +125,15 @@ function updateImages() {
 							 // Show formatted JSON on webpage.
 							 //$("#responseTextArea").val(JSON.stringify(data, null, 2));
 							 var obj = JSON.parse(JSON.stringify(data, null, 2));
-							 console.log(obj.description.captions[0].text);
 							 $(image).attr('alt', obj.description.captions[0].text);
+							 console.log("Injected: " + obj.description.captions[0].text + " into " + $(image).attr('src'));
 					 })
 
 					 .fail(function(jqXHR, textStatus, errorThrown) {
 							 // Display error message.
 							 var errorString = (errorThrown === "") ? "Error. " : errorThrown + " (" + jqXHR.status + "): ";
 							 errorString += (jqXHR.responseText === "") ? "" : jQuery.parseJSON(jqXHR.responseText).message;
-							 console.log(errorString);
+							 //console.log(errorString);
 					 });
 
 				}
